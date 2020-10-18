44d61a3d71099580cc13036bbfd3f30f9801ff22
jleejr70 <jleejr70@yahoo.com>
2020-10-17 20:54:40 -0400

    first commit

---
+++ models/burger.js
@@ -0,0 +1,23 @@
+const orm = require("../config/orm");
+
+
+const burger = {
+    selectAll: function(cb) {
+        orm.selectAll("burgers", function(res) {
+          cb(res);
+        });
+    },
+    insertOne: function(cols, vals, cb) {
+        console.log('vals higher up!!!!:', vals)
+        orm.insertOne("burgers", cols, vals, function(res) {
+            cb(res);
+        });
+    },
+    updateOne: function(objColVals, condition, cb) {
+    orm.updateOne("burgers", objColVals, condition, function(res) {
+        cb(res);
+    });
+    }
+};
+
+module.exports = burger;
